7c97751fe929ede2ada7efa7c21b723fe0e0c32a
chandranidhar <chandrani.influxiq@gmail.com>
2020-05-08 16:13:22 +0530

    image not showing

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('lampconsulting_landing_page app is running!');
+    expect(page.getTitleText()).toEqual('lampconsulting-landing app is running!');
   });
 
   afterEach(async () => {
